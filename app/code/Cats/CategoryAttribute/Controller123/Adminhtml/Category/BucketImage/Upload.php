de8fbc58c1571ece399a4ab8cbeb398a6860836d
amc <amc@amc-con.magemojo.com>
2016-09-17 13:13:12 +0000

    First Commit

---
+++ app/code/Cats/CategoryAttribute/Controller123/Adminhtml/Category/BucketImage/Upload.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Cats\CategoryAttribute\Controller\Adminhtml\Category\BucketImage;
+
+use Magento\Framework\Controller\ResultFactory;
+
+/**
+ * Class Upload
+ */
+class Upload extends \Magento\Backend\App\Action
+{
+    /**
+     * Image uploader
+     *
+     * @var \Magento\Catalog\Model\ImageUploader
+     */
+    protected $imageUploader;
+
+    /**
+     * Upload constructor.
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Catalog\Model\ImageUploader $imageUploader
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Catalog\Model\ImageUploader $imageUploader
+    ) {
+        parent::__construct($context);
+        $this->imageUploader = $imageUploader;
+    }
+
+    /**
+     * Upload file controller action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        try {
+            $result = $this->imageUploader->saveFileToTmpDir('bucket_image');
+
+            $result['cookie'] = [
+                'name' => $this->_getSession()->getName(),
+                'value' => $this->_getSession()->getSessionId(),
+                'lifetime' => $this->_getSession()->getCookieLifetime(),
+                'path' => $this->_getSession()->getCookiePath(),
+                'domain' => $this->_getSession()->getCookieDomain(),
+            ];
+        } catch (\Exception $e) {
+            $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
+        }
+        return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
+    }
+}
---
+++ app/code/Cats/CategoryAttribute/Setup/InstallData.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Cats\CategoryAttribute\Setup;
+ 
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+ 
+class InstallData implements InstallDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+ 
+    /**
+     * Init
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+ 
+    /**
+     * {@inheritdoc}
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+ 
+        /** @var \Magento\Eav\Setup\EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+ 
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'bucket_image',
+            [
+                'type' => 'varchar',
+                'input' => 'image',
+                'label' => 'Bucket Image',
+                'backend' => 'Magento\Catalog\Model\Category\Attribute\Backend\Image',
+                'required' => false,
+                'sort_order' => 5,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'group' => 'General Information',
+            ]
+        );
+ 
+        $setup->endSetup();
+    }
+}
